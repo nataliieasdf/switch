0771ad1f45bef5604ee891e876c8562c12bcbd53
Graphic <graphic@SOD-SJH-516-03M.local>
2015-10-02 14:21:56 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+$('button').on('click', function(e){
 
+	var button = $('button').hasClass('on')
+
+	if (button == true){
+		$('button').removeClass('on').addClass('off');
+		$('body').removeClass('light').addClass('dark');
+		$('.status').text("Hey, who turned off the lights?");
+	}
+	else if (button == false){
+		$('button').removeClass('off').addClass('on');
+		$('body').removeClass('dark').addClass('light');
+		$('.status').text("It's so bright in here!");
+	}
+
+});
 
